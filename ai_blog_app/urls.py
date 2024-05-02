94b849dd23b606eebfe109938cd6de6f7652370d
Elizabeth Wilson <lizwi@192.168.254.152>
2024-05-02 17:57:22 +0800

    Fix internal server error

--- ai_blog_app/urls.py
+++ ai_blog_app/urls.py
@@ -18,12 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_generator.urls')),
-    path('', index, name='index'),
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
