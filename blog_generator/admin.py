b40644d6397c19ddf391ad3f51638c24b9324be1
Elizabeth Wilson <lizwi@192.168.254.152>
2024-05-14 08:18:48 +0800

    Minor change to admin file

--- blog_generator/admin.py
+++ blog_generator/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BlogPost
+from . import models
+# from .models import BlogPost
 
 # Register your models here.
-admin.site.register(BlogPost)
+admin.site.register(models.BlogPost)
